6a90e50a46bcb392d0965f51841f8f940566d61f
Albert-Ghumashyan <albertghumashyan26@gmail.com>
2026-02-10 10:38:48 -0600

    Add Figma project

---
+++ js/script.js
@@ -0,0 +1,39 @@
+function initSlider(sliderSelector, dotsSelector) {
+  let index = 0;
+
+  const slides = document.querySelector(sliderSelector);
+  const dots = document.querySelectorAll(dotsSelector);
+  const totalSlides = dots.length;
+
+  function showSlide(n) {
+    index = n;
+    const nextIndex = (n + 1) % totalSlides;
+
+    slides.style.transform = `translateX(-${n * 100}%)`;
+
+    dots.forEach(dot => {
+      dot.classList.remove('active', 'active-next');
+    });
+
+    if(dots[n].classList[1] !== "dot-one"){
+       dots[n].classList.add('active');
+       dots[nextIndex].classList.add('active-next')
+
+       if(index + 1 == totalSlides){
+        dots[nextIndex].classList.remove('active-next');
+       }
+       
+    } else {
+        dots[n].classList.add('active');
+    }
+  }
+
+  dots.forEach((dot, i) => {
+    dot.addEventListener('click', () => showSlide(i));
+  });
+
+  showSlide(0);
+}
+
+initSlider('.slides_one', '.dots-one .dot');
+initSlider('.slides_two', '.dots-two .dot');
